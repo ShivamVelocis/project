2e965803991075608703b2355184eea3434a4ee6
Shivam <shivam.chaudhry@velocis.co.in>
2021-06-30 14:18:51 +0530

    fixes

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", path.join(__dirname, "src/views/layouts/layout"));
 
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
