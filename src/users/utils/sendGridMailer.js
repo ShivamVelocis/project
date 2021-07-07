a1201846ee4a1af08d31fd759408c1ba76576a5f
Shivam <shivam.chaudhry@velocis.co.in>
2021-07-07 15:57:40 +0530

    mail fixes

--- src/users/utils/sendGridMailer.js
+++ src/users/utils/sendGridMailer.js
@@ -2,17 +2,12 @@ const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 sendOtpMail = async (mailTo, otp, token) => {
-  let forgetPasswordUrl = `${process.env.APP_URL}/user/pwdreset/${token}`;
-
-  let emailBody = `<div><a href="${forgetPasswordUrl}" >Forget password</a></div>
-    <div id="div2">OTP  ${otp} will expire in 10 min.</div>`;
-
-
+  let forgetPasswordUrl = `"${process.env.APP_URL}/user/pwdreset/${token}"`;
+  let emailBody = `<div><a href=${forgetPasswordUrl}>Forget password</a></div><div id="div2">OTP  ${otp} will expire in 10 min.</div>`;
   const msg = {
-    to: mailTo, // Change to your recipient
-    from: process.env.MAIL_SENDR_EMAIL, // Change to your verified sender
+    to: mailTo, 
+    from: process.env.MAIL_SENDR_EMAIL, 
     subject: process.env.OTP_MAIL_SUBJECT,
-    // text: 'and easy to do anywhere, even with Node.js',
     html: emailBody,
   };
   await sgMail.send(msg);
