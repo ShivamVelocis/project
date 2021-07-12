ef1f7fada82b062924959a0a9d71e42dd2bd969f
Shivam <shivam.chaudhry@velocis.co.in>
2021-07-12 22:15:20 +0530

    view acl view add

--- src/middlewares/ACL/Routes/aclRoutes.js
+++ src/middlewares/ACL/Routes/aclRoutes.js
@@ -1,12 +1,12 @@
-const aclController = require('../contollers/aclController')
+const aclController = require("../contollers/aclController");
 const express = require("express");
 const router = express.Router();
 
-router.get("/",aclController.getAcls)
-router.get("/:id",aclController.getAcl)
-router.post('/add',aclController.addACl); 
-router.post("/edit/:id",aclController.editACl);
-router.post("/delete/:id",aclController.deletACl); 
-
+router.get("/", aclController.getAcls);
+router.get("/add", aclController.addACl);
+router.post("/add", aclController.postAddACl);
+router.get("/:id", aclController.getAcl);
+router.post("/edit/:id", aclController.postEditACl);
+router.post("/delete/:id", aclController.postDeletACl);
 
 module.exports = router;
--- src/middlewares/ACL/contollers/aclController.js
+++ src/middlewares/ACL/contollers/aclController.js
@@ -4,7 +4,12 @@ const getAcl = async (req, res, next) => {
   aclId = req.params.id;
   try {
     let result = await aclModel.findById(aclId);
-    res.send(result);
+    console.log(result)
+    return res.render("middlewares/acl/views/view", {
+      title: "ACL rule",
+      module_title: "ACL Mangement",
+      results: result,
+    });
   } catch (error) {
     res.send(error);
     console.log(error);
@@ -23,18 +28,53 @@ const getAcls = async (req, res, next) => {
 };
 
 const addACl = async (req, res, next) => {
-  aclData = req.body;
-  let acl = new aclModel(aclData);
   try {
-    await acl.save();
-    res.send("added");
+    return res.render("middlewares/ACL/views/add", {
+      title: "Add ACL rule",
+      module_title: "ACL Mangement",
+      results: null,
+    });
   } catch (error) {
     res.send(error);
     console.log(error);
   }
 };
 
-const editACl = async (req, res, next) => {
+const postAddACl = async (req, res, next) => {
+  let aclData = {};
+  let allowedResources = [];
+  let denyResources = [];
+
+  try {
+    for (const [key, value] of Object.entries(req.body)) {
+      let methods = [];
+      let path = "";
+      if (key.startsWith("resource")) {
+        let lastDigit = key.split("").pop();
+        path = value;
+        if (Array.isArray(req.body[`methods${lastDigit}`])) {
+          methods = req.body[`methods${lastDigit}`];
+        } else {
+          methods.push(req.body[`methods${lastDigit}`]);
+        }
+        if (req.body["status"] == "allowedResources") {
+          allowedResources.push({ path, methods });
+        } else {
+          denyResources.push({ path, methods });
+        }
+      }
+    }
+    aclData = { allowedResources, denyResources, role: req.body["role"] };
+    let acl = new aclModel(aclData);
+    await acl.save();
+    res.send("added");
+  } catch (error) {
+    next(error);
+    console.log(error);
+  }
+};
+
+const postEditACl = async (req, res, next) => {
   aclId = req.params.id;
   aclData = req.body;
   console.log(aclData);
@@ -51,7 +91,7 @@ const editACl = async (req, res, next) => {
   }
 };
 
-const deletACl = async (req, res, next) => {
+const postDeletACl = async (req, res, next) => {
   aclId = req.params.id;
   aclData = req.body;
   try {
@@ -63,4 +103,11 @@ const deletACl = async (req, res, next) => {
   }
 };
 
-module.exports = { addACl, editACl, deletACl, getAcls, getAcl };
+module.exports = {
+  postAddACl,
+  postEditACl,
+  postDeletACl,
+  getAcls,
+  getAcl,
+  addACl,
+};
