d08e8d982b3780824f509e79014f3e53eb38f347
Shivam <shivam.chaudhry@velocis.co.in>
2021-07-18 19:30:11 +0530

    Update roleTest.js

--- src/ACL/Utils/roleTest.js
+++ src/ACL/Utils/roleTest.js
@@ -136,7 +136,7 @@ const isPermitted = async (req, res, next) => {
   if (isAllowed) return next();
 
   if (req.headers.referer && !req.headers.referer.endsWith(req.originalUrl)) {
-    console.log( req.flash('error'))
+    // console.log( req.flash('error'))
     req.flash("error", CONFIG.AUTH_FAIL_MESSAGE);
     return res.redirect(req.headers.referer);
   } else {
