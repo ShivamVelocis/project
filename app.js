a15b7389fb87b62f72c46b679712f35c9a525f35
Shivam <shivam.chaudhry@velocis.co.in>
2021-07-19 12:04:21 +0530

    script path add

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.set("view engine", "ejs");
 
 app.use(expressLayouts);
 app.set("layout", path.join(__dirname, "src/views/layouts/layout"));
+app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded());
