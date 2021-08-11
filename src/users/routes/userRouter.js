ba361c54c4ab67fe04c427ca5b0d88a0331250fd
Shivam <shivam.chaudhry@velocis.co.in>
2021-08-11 18:25:31 +0530

    Update userRouter.js

--- src/users/routes/userRouter.js
+++ src/users/routes/userRouter.js
@@ -29,7 +29,7 @@ router.post('/change-my-password/',Validtor.changeMyPasswordRule(),Validtor.isRe
 
 
 //Profile Picture
-router.get('/profile/',Validtor.getUserProfileRule(),Validtor.isRequestValid,userController.getProfilePicture)
+router.get('/profile/',userController.getProfilePicture)
 router.post('/profile/',uploadProfilePicture,userController.uploadProfilePicture)
 
 
