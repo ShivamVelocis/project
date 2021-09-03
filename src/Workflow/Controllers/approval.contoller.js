2781e56f48a1859eba7633cb0a9ce84b08e9d4c0
Shivam <shivam.chaudhry@velocis.co.in>
2021-09-03 11:06:43 +0530

    Update approval.contoller.js

--- src/Workflow/Controllers/approval.contoller.js
+++ src/Workflow/Controllers/approval.contoller.js
@@ -206,7 +206,17 @@ const getWfStatu = async (req, res, next) => {
   console.log(req.params.id);
   try {
     let approvalData = await ApprovalModel.findOne({ id: req.params.id });
+    let workflowData = await WorkflowModel.findOne({
+      module: approvalData.module,
+    });
     // console.log(approvalData);
+    let stateName;
+    workflowData.states.map((state) => {
+      if (state.wfLevel == approvalData.level) {
+        stateName = state.stateName;
+      }
+    });
+
     if (!approvalData) {
       return res.json({
         success: false,
@@ -221,7 +231,7 @@ const getWfStatu = async (req, res, next) => {
       data: {
         levelName: approvalData.level,
         comment: approvalData.comment,
-        Status: approvalData.stateName,
+        Status: stateName,
       },
       accesstoken: req.accesstoken,
     });
