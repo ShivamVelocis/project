63ae94773da30db84864381b833b4a1065ce4c4d
Shivam <shivam.chaudhry@velocis.co.in>
2021-09-09 11:29:09 +0530

    Create cache.js

---
+++ src/configs/cache.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const redis = require('redis')
+const util = require('util')
+
+const redisUrl = 'redis://127.0.0.1:6379'
+const client = redis.createClient(redisUrl)
+client.get = util.promisify(client.get)
+client.hget = util.promisify(client.hget)
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.cache = function () {
+    this._useCache = true
+    return this
+}
+
+mongoose.Query.prototype.exec = async function () {
+    if (!this._useCache) {
+        return exec.apply(this, arguments)
+    }
+    const key = JSON.stringify(Object.assign({}, this.getQuery(), { collection: this.mongooseCollection.name }))
+    const cacheValue = await client.get(key)
+    if (cacheValue) {
+        let doc = JSON.parse(cacheValue)
+        return Array.isArray(doc) ? doc.map(d => new this.model(d)) : new this.model(doc)
+    }
+    const result = await exec.apply(this, arguments)
+
+    client.set(key, JSON.stringify(result), "EX", 300)
+}
